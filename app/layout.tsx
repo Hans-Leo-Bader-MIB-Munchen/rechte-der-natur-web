548590099af31f1ff45bad95e584c26f78e45c2a
mucmib <mucmib@gmail.com>
2026-08-09 16:28:50 +0200

    fix(layout): provide global top anchor

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="de">
-      <body>
+      <body id="top">
         {children}
         <SiteFooter />
       </body>
