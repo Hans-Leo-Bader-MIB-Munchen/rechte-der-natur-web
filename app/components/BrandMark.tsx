2dde962366182841f9ab38ab29b591e2139959c7
mucmib <mucmib@gmail.com>
2026-08-09 10:32:59 +0200

    Preview: replace CSS brand mark with official vertical logo

--- app/components/BrandMark.tsx
+++ app/components/BrandMark.tsx
@@ -1,11 +1,14 @@
+import Image from "next/image";
+
 export default function BrandMark() {
   return (
-    <span className="brandMark" aria-hidden="true">
-      <i />
-      <i />
-      <i />
-      <i />
-      <b>⌁</b>
+    <span className="brandMark brandMarkOfficial">
+      <Image
+        src="/brand/logo_D_hochkant.png"
+        alt="Rechte der Natur – Die Volksbegehren"
+        width={541}
+        height={600}
+      />
     </span>
   );
 }
