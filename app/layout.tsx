66ddc6f6aa4b8717bfdd755fbf3615bf59dabb5a
mucmib <mucmib@gmail.com>
2026-08-10 11:09:03 +0200

    Correct OpenGraph image dimensions

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,8 +34,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/brand/OpenGraphGrafik.png",
-        width: 1200,
-        height: 630,
+        width: 1731,
+        height: 909,
         alt: "Rechte der Natur – Die Volksbegehren – rechtedernatur.de",
       },
     ],
