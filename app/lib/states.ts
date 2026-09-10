d77d037033019388c6d0c9d5a328aba4971403fd
Hans Leo Bader <mucmib@gmail.com>
2026-09-10 12:28:07 +0200

    Setze geprüften Normstand für alle Länder

--- app/lib/states.ts
+++ app/lib/states.ts
@@ -1,23 +1,22 @@
 export type StateStatus =
   | "Aktive Sammlung"
-  | "Vorarbeit vorhanden"
-  | "Startpunkt";
+  | "Geprüfter Normstand";
 
 export const federalStates: { name: string; status: StateStatus }[] = [
-  { name: "Baden-Württemberg", status: "Vorarbeit vorhanden" },
+  { name: "Baden-Württemberg", status: "Geprüfter Normstand" },
   { name: "Bayern", status: "Aktive Sammlung" },
-  { name: "Berlin", status: "Vorarbeit vorhanden" },
-  { name: "Brandenburg", status: "Startpunkt" },
-  { name: "Bremen", status: "Startpunkt" },
-  { name: "Hamburg", status: "Vorarbeit vorhanden" },
-  { name: "Hessen", status: "Startpunkt" },
-  { name: "Mecklenburg-Vorpommern", status: "Startpunkt" },
-  { name: "Niedersachsen", status: "Vorarbeit vorhanden" },
-  { name: "Nordrhein-Westfalen", status: "Startpunkt" },
-  { name: "Rheinland-Pfalz", status: "Startpunkt" },
-  { name: "Saarland", status: "Startpunkt" },
-  { name: "Sachsen", status: "Vorarbeit vorhanden" },
-  { name: "Sachsen-Anhalt", status: "Startpunkt" },
-  { name: "Schleswig-Holstein", status: "Startpunkt" },
-  { name: "Thüringen", status: "Vorarbeit vorhanden" },
+  { name: "Berlin", status: "Geprüfter Normstand" },
+  { name: "Brandenburg", status: "Geprüfter Normstand" },
+  { name: "Bremen", status: "Geprüfter Normstand" },
+  { name: "Hamburg", status: "Geprüfter Normstand" },
+  { name: "Hessen", status: "Geprüfter Normstand" },
+  { name: "Mecklenburg-Vorpommern", status: "Geprüfter Normstand" },
+  { name: "Niedersachsen", status: "Geprüfter Normstand" },
+  { name: "Nordrhein-Westfalen", status: "Geprüfter Normstand" },
+  { name: "Rheinland-Pfalz", status: "Geprüfter Normstand" },
+  { name: "Saarland", status: "Geprüfter Normstand" },
+  { name: "Sachsen", status: "Geprüfter Normstand" },
+  { name: "Sachsen-Anhalt", status: "Geprüfter Normstand" },
+  { name: "Schleswig-Holstein", status: "Geprüfter Normstand" },
+  { name: "Thüringen", status: "Geprüfter Normstand" },
 ];
